0876c43f6525bba6e63e7e97803053cb25f323cb
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-06-11 10:50:31 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from'react';
+import axios from 'axios';
+
+const url = `https://jsonplaceholder.typicode.com/posts`
+const urlId = `https://jsonplaceholder.typicode.com/posts/1`
+
 
 function App() {
+  const [post, setPost] = useState([]);
+  const [error, setError] = useState(null);
+
+   useEffect(() => {
+    axios
+      .get(urlId)
+      .then(response => {
+        setPost(response.data);
+      })
+      .catch(error => {
+        setError(error);      
+      })
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${url}/1`).then(response => {
+        setPost(response.data);
+      }
+    )
+  }, []);
+
+  const createPost = () => {  
+    axios
+      .post(url, {title: 'Post 1', body: 'Post 1'})
+      .then(response => {
+        setPost(response.data);
+      }
+    )
+  }
+
+  const updatePost = () => {
+    axios
+      .put(`${url}/1`, {title: 'Post 2', body: 'Post 2'})
+      .then(response => {
+        setPost(response.data);
+      }
+    )
+  }
+
+  const deletePost = () => {
+    axios
+      .delete(`${url}/1`)
+      .then(response => {
+        setPost(response.data);
+      }
+    )
+  }
+
+
+  if(!post){
+    return(
+      <>
+        <h4>No data.</h4>
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div>
+          {post.title}
+        </div>
+      </>
   );
 }
 
